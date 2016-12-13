92ed61f92ccce2cf98c1801c336456bbde0168b8
ESCAPE\escape <bin_sunshine_7@163.com>
2016-12-13 21:56:06 +0800

    escape v0.7

--- escape/application/index/controller/Index.php
+++ escape/application/index/controller/Index.php
@@ -4,13 +4,20 @@ use think\Controller;
 
 class Index extends Controller
 {
+
     public function index()
     {
+        model('Count')->where('id','>',0)->setInc('visited');
         return $this->fetch();
     }
 
     public function home()
     {
+        $data = [];
+        $data['ip'] = $this->getIP();
+        $data['visited_time'] = time();
+        model('Who')->insert($data);
+        model('Count')->where('id','>',0)->setInc('home');
     	$res = model('Article')->where('id','>',5)->select();
     	$this->assign('article',$res);
     	return $this->fetch();
@@ -18,6 +25,7 @@ class Index extends Controller
 
     public function article($get)
     {
+        model('Count')->where('id','>',0)->setInc('reading');
     	$where = [];
     	$where['id'] = $get;
     	$res = model('Article')->where($where)->find();
@@ -27,10 +35,34 @@ class Index extends Controller
 
     public function about()
     {
+        model('Count')->where('id','>',0)->setInc('about');
     	$where = [];
     	$where['id'] = 5;
     	$res = model('Article')->where($where)->find();
     	$this->assign('res',$res);
     	return $this->fetch();
     }
+
+    function getIP()
+    {
+        if (isset($_SERVER)){
+            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
+                $realip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+            } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+                $realip = $_SERVER["HTTP_CLIENT_IP"];
+            } else {
+                $realip = $_SERVER["REMOTE_ADDR"];
+            }
+        } else {
+            if (getenv("HTTP_X_FORWARDED_FOR")){
+                $realip = getenv("HTTP_X_FORWARDED_FOR");
+            } else if (getenv("HTTP_CLIENT_IP")) {
+                $realip = getenv("HTTP_CLIENT_IP");
+            } else {
+                $realip = getenv("REMOTE_ADDR");
+            }
+        }
+        return $realip;
+    }
+
 }
